402e547330f1afc7ef54b459921fb370b8aa04d4
Daniel Kurek <daniel.kurek.dev@gmail.com>
2023-09-10 19:38:46 +0200

    [logging-example] clean up code

--- src/esp32-logging/main/main.c
+++ src/esp32-logging/main/main.c
@@ -11,21 +11,6 @@
 
 static const char *TAG = "MAIN";
 
-config_option_t options[] ={
-    {
-        .key = "device/name",
-        .type = NVS_TYPE_STR,
-        .value_to_log = true,
-    },
-    {
-        .key = "device/id",
-        .type = NVS_TYPE_I16,
-        .value_to_log = true,
-    },
-};
-
-static const size_t options_len = sizeof(options) / sizeof(options[0]);
-
 /* Event handler for catching system events */
 static void event_handler(void* arg, esp_event_base_t event_base,
                           int32_t event_id, void* event_data)
@@ -91,29 +76,13 @@ void test_logging(void *pvParameters){
     vTaskDelay(5000 / portTICK_PERIOD_MS);
     logger_dump_log_file();
     logger_stop();
-
-    // vTaskDelay(10000 / portTICK_PERIOD_MS);
-    // logger_dump_log_file();
-    // vTaskDelay(2000 / portTICK_PERIOD_MS);
     
-    // for(int i = 0; i < 568; i++){
-    //     LOGGER_E("LOGGER_TEST", "%d Hello world %d!", i, i);
-    //     vTaskDelay(50 / portTICK_PERIOD_MS);
-    // }
-
-    // vTaskDelay(2000 / portTICK_PERIOD_MS);
-    // logger_dump_log_file();
-    // vTaskDelay(2000 / portTICK_PERIOD_MS);
-    // logger_stop();
     vTaskDelete(NULL);
 }
 
 
 void app_main(void)
 {
-    // xTaskCreate(&ota_task, "ota_task", 1024 * 8, NULL, 5, NULL);
-    web_config_set_custom_options(options_len, options);
-    web_config_start();
     // avoid watchdog for IDLE task (needs to run once in a while)
-    // xTaskCreate(test_logging, "Logging Test", 1024 * 8, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(test_logging, "Logging Test", 1024 * 8, NULL, tskIDLE_PRIORITY, NULL);
 }
