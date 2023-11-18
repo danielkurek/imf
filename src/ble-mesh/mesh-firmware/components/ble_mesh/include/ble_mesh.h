b5ec9e8557a0bd681fda63a97be0850eaa55de2b
Daniel Kurek <daniel.kurek.dev@gmail.com>
2023-11-18 10:37:29 +0100

    [mesh firmware] ble_mesh add c++ support

--- src/ble-mesh/mesh-firmware/components/ble_mesh/include/ble_mesh.h
+++ src/ble-mesh/mesh-firmware/components/ble_mesh/include/ble_mesh.h
@@ -6,6 +6,11 @@
 #include "board.h"
 #include "rgb_control_client.h"
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
 esp_err_t ble_mesh_init();
 
 void ble_mesh_set_rgb_conf(rgb_conf_t *conf);
@@ -13,5 +18,9 @@ void ble_mesh_set_rgb_conf(rgb_conf_t *conf);
 esp_err_t ble_mesh_set_rgb(uint16_t addr, rgb_t color, bool ack);
 
 rgb_response_t ble_mesh_get_rgb(uint16_t addr);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
