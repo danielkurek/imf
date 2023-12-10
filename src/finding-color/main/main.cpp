7983fbdeadd63751f1d75dbd19706b7199aa8905
Daniel Kurek <daniel.kurek.dev@gmail.com>
2023-12-10 16:46:19 +0100

    [finding color] add color logic

--- src/finding-color/main/main.cpp
+++ src/finding-color/main/main.cpp
@@ -5,9 +5,12 @@
 #include "nvs_flash.h"
 #include "esp_check.h"
 #include "esp_event.h"
+#include "esp_random.h"
 
 #include "interactive-mesh-framework.hpp"
 #include "driver/uart.h"
+
+#include <vector>
 
 #ifdef CONFIG_IDF_TARGET_ESP32C3
 #define CONFIG_BUTTON GPIO_NUM_9
@@ -23,13 +26,51 @@ static IMF s_imf;
 static std::shared_ptr<Device> closest_device;
 static TickType_t closest_timestamp_ms;
 
+static std::vector<rgb_t> colors;
+static size_t current_color_index;
+
 constexpr uint32_t closest_time_limit = 10000;
+
+constexpr int16_t color_cmp_threshold = 10;
+
+size_t random_number(size_t max){
+    uint32_t rnd = esp_random();
+    return rnd % max;
+}
+
+void new_color(){
+    current_color_index = random_number(colors.size());
+    this_device->setRgb(colors[current_color_index]);
+}
+
+bool colors_equal(const rgb_t *c1, const rgb_t *c2){
+    if(abs((int16_t) c1->red - (int16_t) c2->red) > color_cmp_threshold){
+        return false;
+    }
+    if(abs((int16_t) c1->green - (int16_t) c2->green) > color_cmp_threshold){
+        return false;
+    }
+    if(abs((int16_t) c1->blue - (int16_t) c2->blue) > color_cmp_threshold){
+        return false;
+    }
+    return true;
+}
+
+void check_color(){
+    if(closest_device == nullptr){
+        return;
+    }
+    rgb_t remote_color = closest_device->getRgb();
+    if(colors_equal(&remote_color, &colors[current_color_index])){
+        new_color();
+    }
+}
 
 void update_cb(TickType_t diff_ms){
     ESP_LOGI(TAG, "Time since last update: %lu", diff_ms);
     TickType_t now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
     if((now_ms - closest_timestamp_ms) >= closest_time_limit){
-        // check color
+        check_color();
     }
 }
 
@@ -57,13 +98,13 @@ extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_ba
                 // check if the event is newer than what is saved
                 // does not hold when timestamp overflows 
                 // but it is ok, since it happens after more than 1000 hours (way more than expected runtime)
-                if(dm_nearest_dev->closest_timestamp_ms >= closest_timestamp_ms){
+                if(dm_nearest_dev->timestamp_ms >= closest_timestamp_ms){
                     if(dm_nearest_dev->new_point_id == UINT32_MAX){
                         closest_device = nullptr;
                     } else{
                         closest_device = s_imf.getDevice(dm_nearest_dev->new_point_id);
                     }
-                    closest_timestamp_ms = dm_nearest_dev->closest_timestamp_ms;
+                    closest_timestamp_ms = dm_nearest_dev->timestamp_ms;
                 }
                 break;
             default:
@@ -89,13 +130,28 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     ESP_LOGI(TAG, "init IMF");
-    
-    
+
     ESP_LOGI(TAG, "IMF add devices");
-    s_imf.addDevice(DeviceType::Station, "34:b4:72:6a:77:c1", 1, 0xc000);
+    uint32_t id = s_imf.addDevice(DeviceType::Station, "34:b4:72:6a:77:c1", 1, 0xc000);
+    auto device = s_imf.getDevice(id);
+    if(device != nullptr){
+        rgb_t color = device->getRgb();
+        bool add_color = true;
+        for(auto &&_color : colors){
+            if(_color.red == color.red && _color.green == color.green && _color.blue == color.blue){
+                add_color = false;
+                break;
+            }
+        }
+        if(add_color){
+            colors.push_back(color);
+        }
+    }
     
     ESP_LOGI(TAG, "IMF register callbacks");
     s_imf.registerCallbacks(button_cb, event_handler, NULL, update_cb);
+
+    new_color();
 
     ESP_LOGI(TAG, "IMF start");
     s_imf.start();
