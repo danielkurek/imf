251cf9c2956656041223c342a3f6ab10d403ea9f
Daniel Kurek <daniel.kurek.dev@gmail.com>
2024-02-11 11:36:03 +0100

    [color] stricter color parsing from string

--- src/common-components/color/color.c
+++ src/common-components/color/color.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "color/color.h"
 #include <inttypes.h>
+#include <string.h>
 
 esp_err_t rgb_to_str(rgb_t color, size_t buf_len, char buf[]){
     int ret = snprintf(buf, buf_len, "%02" PRIx8 "%02" PRIx8 "%02" PRIx8, color.red, color.green, color.blue);
@@ -11,6 +12,8 @@ esp_err_t rgb_to_str(rgb_t color, size_t buf_len, char buf[]){
 }
 
 esp_err_t str_to_rgb(const char *buf, rgb_t *color){
+    if(strlen(buf) != 6) return ESP_FAIL;
+
     int ret = sscanf(buf, "%02" SCNx8 "%02" SCNx8 "%02" SCNx8, &(color->red), &(color->green), &(color->blue));
     if(ret == 3){
         return ESP_OK;
