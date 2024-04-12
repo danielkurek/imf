3bb8bb5a1adeae4ad8e896f234fc8623ad0bce72
Daniel Kurek <daniel.kurek.dev@gmail.com>
2024-04-12 13:08:34 +0200

    [finding-color] update example projects

--- src/finding-color/finding-color-mobile/main/main.cpp
+++ src/finding-color/finding-color-mobile/main/main.cpp
@@ -39,12 +39,14 @@ static const std::vector<button_gpio_config_t> buttons = {
     {.gpio_num = GPIO_NUM_47, .active_level = 1},
     {.gpio_num = GPIO_NUM_48, .active_level = 1},
     {.gpio_num = GPIO_NUM_45, .active_level = 1},
+    {.gpio_num = GPIO_NUM_0 , .active_level = 0},
 };
 
 typedef struct {
     std::string mac;
     uint16_t ble_addr;
     uint16_t wifi_channel;
+    DeviceType type;
 } device_conf_t;
 
 size_t random_number(size_t max){
@@ -52,10 +54,10 @@ size_t random_number(size_t max){
     return rnd % max;
 }
 
-void new_color(){
+bool new_color(){
     if(colors.size() == 0){
         LOGGER_E(TAG, "No colors to choose from!");
-        return;
+        return false;
     }
     current_color_index = random_number(colors.size());
     LOGGER_I(TAG, "Choosing index=%d as new color", current_color_index);
@@ -63,14 +65,16 @@ void new_color(){
     esp_err_t err = rgb_to_str(colors[current_color_index], 6+1, rgb_str);
     if(err != ESP_OK){
         LOGGER_E(TAG, "Could not convert new color to string! Err: %d", err);
+        return false;
     } else{
         LOGGER_I(TAG, "Setting new color to %s", rgb_str);
     }
     if(Device::this_device == nullptr){
         LOGGER_E(TAG, "Local device is not initialized, could not set RGB");
-        return;
+        return false;
     }
-    Device::this_device->setRgb(colors[current_color_index]);
+    err = Device::this_device->setRgb(colors[current_color_index]);
+    return err == ESP_OK;
 }
 
 bool colors_equal(const rgb_t *c1, const rgb_t *c2){
@@ -86,31 +90,46 @@ bool colors_equal(const rgb_t *c1, const rgb_t *c2){
     return true;
 }
 
-void check_color(){
+bool check_color(){
     if(closest_device == nullptr){
-        return;
+        return false;
     }
     rgb_t remote_color;
-    esp_err_t err = closest_device->getRgb(&remote_color);
+    esp_err_t err = closest_device->getRgb(remote_color);
     if(err != ESP_OK){
         LOGGER_E(TAG, "Could not get RGB of closest device 0x%04" PRIx16, closest_device->ble_mesh_addr);
-        return;
+        return false;
     }
     if(colors_equal(&remote_color, &colors[current_color_index])){
-        new_color();
+        return new_color();
     }
+    return false;
 }
 
 void update_cb(TickType_t diff_ms){
     ESP_LOGI(TAG, "Time since last update: %lu", diff_ms);
     TickType_t now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
     if((now_ms - closest_timestamp_ms) >= closest_time_limit){
-        check_color();
+        // check_color();
+        if(check_color()){
+            closest_timestamp_ms = now_ms;
+        }
     }
 }
 
 extern "C" void button_cb(uint8_t button_num){
     ESP_LOGI(TAG, "Button no. %d was pressed", button_num);
+    if(button_num == 4){
+        int16_t level = 0;
+        if(Device::this_device){
+            if(ESP_OK != Device::this_device->getLevel(level)){
+                level = 0;
+            }
+            Device::this_device->setLevel(level+1);
+        } else{
+            LOGGER_E(TAG, "This device is not initialized!");
+        }
+    }
 }
 
 extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_base,
@@ -185,26 +204,29 @@ extern "C" void app_main(void)
         return;
     }
 
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
     ESP_LOGI(TAG, "init IMF");
 
     ESP_LOGI(TAG, "IMF add devices");
     const device_conf_t devices_confs[] = {
-        {"34:b4:72:69:cb:9d", 0xc000, 1},
-        {"34:b4:72:69:e6:13", 0xc001, 1},
-        {"34:b4:72:6a:76:ed", 0xc002, 1},
-        {"34:b4:72:6a:77:c1", 0xc003, 1},
-        {"34:b4:72:6a:84:59", 0xc004, 1},
+        {"F4:12:FA:EA:0B:91", 0x0002, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:85", 0x0005, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:65", 0x0008, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:75", 0x000B, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:A5", 0x0011, 1, DeviceType::Mobile },
+        {"F4:12:FA:EA:0B:81", 0x0014, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:F9", 0x0017, 1, DeviceType::Station},
+        {"F4:12:FA:E8:D2:6D", 0x001A, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:89", 0x001D, 1, DeviceType::Station},
+        {"F4:12:FA:E8:D3:6D", 0x0020, 1, DeviceType::Mobile },
     };
     const size_t devices_len = sizeof(devices_confs) / sizeof(devices_confs[0]);
     for(size_t i = 0; i < devices_len; i++){
         const device_conf_t *d_conf = &devices_confs[i];
-        uint32_t id = s_imf->addDevice(DeviceType::Station, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
+        uint32_t id = s_imf->addDevice(d_conf->type, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
         std::shared_ptr<Device> device = s_imf->getDevice(id);
         if(device != nullptr){
             rgb_t color;
-            esp_err_t err = device->getRgb(&color);
+            esp_err_t err = device->getRgb(color);
             if(err == ESP_OK){
                 bool add_color = true;
                 for(auto &&_color : colors){
@@ -230,9 +252,9 @@ extern "C" void app_main(void)
         } else
             colors_str += "," + std::string(rgb_str);
     }
-    ESP_LOGI(TAG, "Using colors: %s", colors_str.c_str());
+    LOGGER_I(TAG, "Using colors: %s", colors_str.c_str());
     ESP_LOGI(TAG, "IMF register callbacks");
-    s_imf->registerCallbacks(button_cb, event_handler, NULL, update_cb);
+    s_imf->registerCallbacks(button_cb, event_handler, NULL, update_cb, nullptr);
 
     ESP_LOGI(TAG, "IMF start");
     s_imf->start();
--- src/finding-color/finding-color-station/main/main.cpp
+++ src/finding-color/finding-color-station/main/main.cpp
@@ -30,12 +30,14 @@ static const std::vector<button_gpio_config_t> buttons = {
     {.gpio_num = GPIO_NUM_47, .active_level = 1},
     {.gpio_num = GPIO_NUM_48, .active_level = 1},
     {.gpio_num = GPIO_NUM_45, .active_level = 1},
+    {.gpio_num = GPIO_NUM_0 , .active_level = 0},
 };
 
 typedef struct {
     std::string mac;
     uint16_t ble_addr;
     uint16_t wifi_channel;
+    DeviceType type;
 } device_conf_t;
 
 size_t random_number(size_t max){
@@ -45,6 +47,31 @@ size_t random_number(size_t max){
 
 extern "C" void button_cb(uint8_t button_num){
     ESP_LOGI(TAG, "Button no. %d was pressed", button_num);
+    if(button_num == 0){
+        if(s_imf){
+            LOGGER_I(TAG, "Starting location topology");
+            s_imf->startLocalization();
+        }
+    } else if(button_num == 1){
+        int16_t level = 0;
+        if(Device::this_device && ESP_OK != Device::this_device->getLevel(level)){
+            level = 0;
+        }
+        if(ESP_OK != Device::setLevelAll(level+1)){
+            LOGGER_E(TAG, "Could not set level!");
+        }
+    }
+    else if(button_num == 4){
+        int16_t level = 0;
+        if(Device::this_device){
+            if(ESP_OK != Device::this_device->getLevel(level)){
+                level = 0;
+            }
+            Device::this_device->setLevel(level+1);
+        } else{
+            LOGGER_E(TAG, "This device is not initialized!");
+        }
+    }
 }
 
 // start configuration mode if conditions are met
@@ -84,26 +111,28 @@ extern "C" void app_main(void)
         return;
     }
 
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
     ESP_LOGI(TAG, "init IMF");
 
     ESP_LOGI(TAG, "IMF add devices");
     const device_conf_t devices_confs[] = {
-        {"34:b4:72:69:cb:9d", 0xc000, 1},
-        {"34:b4:72:69:e6:13", 0xc001, 1},
-        {"34:b4:72:6a:76:ed", 0xc002, 1},
-        {"34:b4:72:6a:77:c1", 0xc003, 1},
-        {"34:b4:72:6a:84:59", 0xc004, 1},
+        {"F4:12:FA:EA:0B:91", 0x0002, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:85", 0x0005, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:65", 0x0008, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:75", 0x000B, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:A5", 0x0011, 1, DeviceType::Mobile },
+        {"F4:12:FA:EA:0B:81", 0x0014, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:F9", 0x0017, 1, DeviceType::Station},
+        {"F4:12:FA:E8:D2:6D", 0x001A, 1, DeviceType::Station},
+        {"F4:12:FA:EA:0B:89", 0x001D, 1, DeviceType::Station},
+        {"F4:12:FA:E8:D3:6D", 0x0020, 1, DeviceType::Mobile },
     };
     const size_t devices_len = sizeof(devices_confs) / sizeof(devices_confs[0]);
     for(size_t i = 0; i < devices_len; i++){
         const device_conf_t *d_conf = &devices_confs[i];
-        uint32_t id = s_imf->addDevice(DeviceType::Mobile, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
+        uint32_t id = s_imf->addDevice(d_conf->type, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
     }
     ESP_LOGI(TAG, "IMF register callbacks");
-    s_imf->registerCallbacks(button_cb, nullptr, NULL, nullptr);
+    s_imf->registerCallbacks(button_cb, nullptr, NULL, nullptr, nullptr);
 
     ESP_LOGI(TAG, "IMF start");
     s_imf->start();
