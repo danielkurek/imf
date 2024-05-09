e73ade908e5b5989eca7872bb9e97a2873db45ab
Daniel Kurek <daniel.kurek.dev@gmail.com>
2024-05-09 09:56:01 +0200

    [finding-color] add comments and clean up code

--- src/finding-color/finding-color-mobile/main/main.cpp
+++ src/finding-color/finding-color-mobile/main/main.cpp
@@ -2,13 +2,10 @@
 #include <inttypes.h>
 #include "esp_log.h"
 #include "driver/gpio.h"
-#include "nvs_flash.h"
-#include "esp_check.h"
 #include "esp_event.h"
 #include "esp_random.h"
 
 #include "interactive-mesh-framework.hpp"
-#include "driver/uart.h"
 
 #include <vector>
 #include <memory>
@@ -20,6 +17,7 @@
 #define CONFIG_BUTTON GPIO_NUM_0
 #endif
 
+// Tag for logging messages
 static const char *TAG = "APP";
 
 using namespace imf;
@@ -36,13 +34,10 @@ constexpr TickType_t closest_time_limit_ms = 5000 / portTICK_PERIOD_MS;
 constexpr int16_t color_cmp_threshold = 10;
 
 static const std::vector<button_gpio_config_t> buttons = {
-    {.gpio_num = GPIO_NUM_46, .active_level = 1},
-    {.gpio_num = GPIO_NUM_47, .active_level = 1},
-    {.gpio_num = GPIO_NUM_48, .active_level = 1},
-    {.gpio_num = GPIO_NUM_45, .active_level = 1},
-    {.gpio_num = GPIO_NUM_0 , .active_level = 0},
+    {.gpio_num = GPIO_NUM_0 , .active_level = 0}
 };
 
+// Helping struct to help adding device to IMF
 typedef struct {
     std::string mac;
     uint16_t ble_addr;
@@ -50,6 +45,7 @@ typedef struct {
     DeviceType type;
 } device_conf_t;
 
+// Get colors from all devices and populate `colors` vector
 void update_colors(){
     std::vector<rgb_t> new_colors;
     for(auto it = s_imf->devices_cbegin(); it != s_imf->devices_cend(); it++){
@@ -74,6 +70,7 @@ void update_colors(){
     colors = std::move(new_colors);
 }
 
+// print colors to log
 void print_colors(){
     std::string colors_str;
     bool first = true;
@@ -94,6 +91,7 @@ size_t random_number(size_t max){
     return rnd % max;
 }
 
+// logic for generating new color
 bool new_color(){
     update_colors();
     print_colors();
@@ -123,6 +121,8 @@ bool new_color(){
     return err == ESP_OK;
 }
 
+// Colors after conversion to/from HSL are not precise
+// this function ensures that if colors equal even if they have only small difference between them
 bool colors_equal(const rgb_t &c1, const rgb_t &c2){
     if(std::abs((int16_t) c1.red - (int16_t) c2.red) > color_cmp_threshold){
         return false;
@@ -136,6 +136,7 @@ bool colors_equal(const rgb_t &c1, const rgb_t &c2){
     return true;
 }
 
+// Check that this device is standing by the correct station
 bool check_color(){
     // current color is not valid
     if(colors.size() == 0){
@@ -158,9 +159,11 @@ bool check_color(){
     return false;
 }
 
+// update callback from IMF
 void update_cb(TickType_t diff_ms){
     LOGGER_I(TAG, "Time since last update: %" PRIu32, diff_ms);
     TickType_t now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
+    // check correct color only after we are standing at least closest_time_limit_ms by the same station
     if((now_ms - closest_timestamp_ms) >= closest_time_limit_ms){
         if(check_color()){
             closest_timestamp_ms = now_ms;
@@ -168,11 +171,14 @@ void update_cb(TickType_t diff_ms){
     }
 }
 
+// button callback from IMF
 extern "C" void button_cb(uint8_t button_num){
     LOGGER_I(TAG, "Button no. %d was pressed", button_num);
-    if(button_num == 4){
+    if(button_num == 0){
         int16_t level = 0;
+        // Check if this device is initialized
         if(Device::this_device){
+            // Increment state number
             if(ESP_OK != Device::this_device->getLevel(level)){
                 level = 0;
             }
@@ -183,12 +189,14 @@ extern "C" void button_cb(uint8_t button_num){
     }
 }
 
+// distance event handler from IMF
 extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_base,
                    int32_t event_id, void* event_data){
     if(event_base == DM_EVENT){
         dm_measurement_data_t *dm_measurement;
         dm_nearest_device_change_t *dm_nearest_dev;
         switch(event_id){
+            // only for debugging
             case DM_MEASUREMENT_DONE:
                 dm_measurement = (dm_measurement_data_t*) event_data;
                 if(dm_measurement->valid)
@@ -196,6 +204,7 @@ extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_ba
                 else
                     ESP_LOGI(TAG, "DM_MEASUREMENT_DONE, id=%" PRIu32 ", INVALID", dm_measurement->point_id);
                 break;
+            // main logic
             case DM_NEAREST_DEVICE_CHANGE:
                 dm_nearest_dev = (dm_nearest_device_change_t*) event_data;
                 ESP_LOGI(TAG, "DM_NEAREST_DEVICE_CHANGE, from: %" PRIx32 " | to: %" PRIx32, dm_nearest_dev->old_point_id, dm_nearest_dev->new_point_id);
@@ -219,7 +228,9 @@ extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_ba
 }
 
 // start configuration mode if conditions are met
+// (BOOT button needs to pressed down 1s after start)
 bool web_config(){
+    // Configure GPIO pin that is connected to Boot button
     gpio_config_t config;
 
     config.intr_type    = GPIO_INTR_DISABLE;
@@ -230,27 +241,31 @@ bool web_config(){
 
     gpio_config(&config);
 
+    // Wait 1s
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
+    // Check if button is pressed down
     if(gpio_get_level(CONFIG_BUTTON) == 0){
         LOGGER_I(TAG, "Booting to web config");
         s_imf->startWebConfig();
-        return true;
+        return true; // do not start IMF, configuration mode
     }
-    return false;
+    return false; // continue normally
 }
 
+// initialize and configure framework
 extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "Startup!");
-    esp_err_t err;
 
+    // Create instance of IMF
     s_imf = std::make_unique<IMF>(buttons);
     if(!s_imf){
         ESP_LOGE(TAG, "Could not init IMF!");
         return;
     }
 
+    // check if conditions are met for configuration mode
     if(web_config()){
         return;
     }
@@ -258,6 +273,7 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG, "init IMF");
 
     ESP_LOGI(TAG, "IMF add devices");
+    // change this configuration if you are using your own devices
     const device_conf_t devices_confs[] = {
         {"F4:12:FA:EA:0B:91", 0x0002, 1, DeviceType::Station},
         {"F4:12:FA:EA:0B:85", 0x0005, 1, DeviceType::Station},
@@ -271,6 +287,7 @@ extern "C" void app_main(void)
         {"F4:12:FA:E8:D3:6D", 0x0020, 1, DeviceType::Mobile },
     };
     const size_t devices_len = sizeof(devices_confs) / sizeof(devices_confs[0]);
+    // Add devices one by one
     for(size_t i = 0; i < devices_len; i++){
         const device_conf_t *d_conf = &devices_confs[i];
         uint32_t id = s_imf->addDevice(d_conf->type, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
@@ -280,6 +297,7 @@ extern "C" void app_main(void)
         }
     }
 
+    // request colors from all devices
     update_colors();
     // wait for responses because of lazy loading values
     vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -293,5 +311,6 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG, "IMF start");
     s_imf->start();
     
+    // generate first color
     new_color();
 }
--- src/finding-color/finding-color-station/main/main.cpp
+++ src/finding-color/finding-color-station/main/main.cpp
@@ -2,13 +2,10 @@
 #include <inttypes.h>
 #include "esp_log.h"
 #include "driver/gpio.h"
-#include "nvs_flash.h"
-#include "esp_check.h"
 #include "esp_event.h"
 #include "esp_random.h"
 
 #include "interactive-mesh-framework.hpp"
-#include "driver/uart.h"
 
 #include <vector>
 #include <memory>
@@ -19,6 +16,7 @@
 #define CONFIG_BUTTON GPIO_NUM_0
 #endif
 
+// Tag for logging messages
 static const char *TAG = "APP";
 
 using namespace imf;
@@ -26,13 +24,10 @@ using namespace imf;
 static std::unique_ptr<IMF> s_imf;
 
 static const std::vector<button_gpio_config_t> buttons = {
-    {.gpio_num = GPIO_NUM_46, .active_level = 1},
-    {.gpio_num = GPIO_NUM_47, .active_level = 1},
-    {.gpio_num = GPIO_NUM_48, .active_level = 1},
-    {.gpio_num = GPIO_NUM_45, .active_level = 1},
-    {.gpio_num = GPIO_NUM_0 , .active_level = 0},
+    {.gpio_num = GPIO_NUM_0 , .active_level = 0}
 };
 
+// Helping struct to help adding device to IMF
 typedef struct {
     std::string mac;
     uint16_t ble_addr;
@@ -40,6 +35,8 @@ typedef struct {
     DeviceType type;
 } device_conf_t;
 
+// distance event handler from IMF
+// used only for logging (can be removed if we do not need to log distance measurements)
 extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_base,
                    int32_t event_id, void* event_data){
     if(event_base == DM_EVENT){
@@ -65,25 +62,15 @@ extern "C" void event_handler(void* event_handler_arg, esp_event_base_t event_ba
     }
 }
 
+// button callback from IMF
 extern "C" void button_cb(uint8_t button_num){
     ESP_LOGI(TAG, "Button no. %d was pressed", button_num);
+    // change states
     if(button_num == 0){
-        if(s_imf){
-            LOGGER_I(TAG, "Starting location topology");
-            s_imf->startLocalization();
-        }
-    } else if(button_num == 1){
         int16_t level = 0;
-        if(Device::this_device && ESP_OK != Device::this_device->getLevel(level)){
-            level = 0;
-        }
-        if(ESP_OK != Device::setLevelAll(level+1)){
-            LOGGER_E(TAG, "Could not set level!");
-        }
-    }
-    else if(button_num == 4){
-        int16_t level = 0;
+        // Check if this device is initialized
         if(Device::this_device){
+            // Increment state number
             if(ESP_OK != Device::this_device->getLevel(level)){
                 level = 0;
             }
@@ -95,7 +82,9 @@ extern "C" void button_cb(uint8_t button_num){
 }
 
 // start configuration mode if conditions are met
+// (BOOT button needs to pressed down 1s after start)
 bool web_config(){
+    // Configure GPIO pin that is connected to Boot button
     gpio_config_t config;
 
     config.intr_type    = GPIO_INTR_DISABLE;
@@ -106,20 +95,22 @@ bool web_config(){
 
     gpio_config(&config);
 
+    // Wait 1s
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
+    // Check if button is pressed down
     if(gpio_get_level(CONFIG_BUTTON) == 0){
         LOGGER_I(TAG, "Booting to web config");
         s_imf->startWebConfig();
-        return true;
+        return true; // do not start IMF, configuration mode
     }
-    return false;
+    return false; // continue normally
 }
 
+// initialize and configure framework
 extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "Startup!");
-    esp_err_t err;
     
     s_imf = std::make_unique<IMF>(buttons);
     if(!s_imf){
@@ -134,6 +125,7 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG, "init IMF");
 
     ESP_LOGI(TAG, "IMF add devices");
+    // change this configuration if you are using your own devices
     const device_conf_t devices_confs[] = {
         {"F4:12:FA:EA:0B:91", 0x0002, 1, DeviceType::Station},
         {"F4:12:FA:EA:0B:85", 0x0005, 1, DeviceType::Station},
@@ -147,11 +139,16 @@ extern "C" void app_main(void)
         {"F4:12:FA:E8:D3:6D", 0x0020, 1, DeviceType::Mobile },
     };
     const size_t devices_len = sizeof(devices_confs) / sizeof(devices_confs[0]);
+    // Add devices one by one
     for(size_t i = 0; i < devices_len; i++){
         const device_conf_t *d_conf = &devices_confs[i];
         uint32_t id = s_imf->addDevice(d_conf->type, d_conf->mac, d_conf->wifi_channel, d_conf->ble_addr);
+        if(id == UINT32_MAX){
+            ESP_LOGE(TAG, "Error adding device to IMF");
+        }
     }
     ESP_LOGI(TAG, "IMF register callbacks");
+    // not needed if we do not need button callback and event_handler
     s_imf->registerCallbacks(button_cb, event_handler, NULL, nullptr, nullptr);
 
     ESP_LOGI(TAG, "IMF start");
